c583663b64d93e0bc858c6221df798a2602fedcc
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-10-24 03:06:22 +0300

    Implement solution

--- task.sql
+++ task.sql
@@ -1,14 +1,12 @@
 -- Use our database
 USE ShopDB;
-
-INSERT INTO Orders (CustomerID, Date)
-    VALUES (1, '2023-01-01');
-
 -- Start the transaction
 START TRANSACTION;
 
+INSERT INTO Orders (CustomerID, Date)
+    VALUES (1, '2023-01-01');
 INSERT INTO OrderItems (OrderID, ProductID, Count)
-    VALUES (1, 1, 1);
+    VALUES ( LAST_INSERT_ID(), 1, 1);
 UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = '1' AND WarehouseAmount >= 1;
 
 COMMIT;
